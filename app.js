2e7d9cbb97dc85701296002774e41ae700e45d03
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 03:05:55 -0300

    created userPosition endpoint

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ app.use(bodyParser.json())
 app.get('/', (req, res, next) => {
     res.json({message: "API is working"});
 })
+
+app.get('/userPosition', auth.verifyJWT, (req, res, next) => { 
+    let userPosition = {
+        "checkingAccountAmount": 234.00, // Saldo em conta corrente
+        "positions": [
+            {
+                "symbol": "PETR4",
+                "amount": 2,
+                "currentPrice": 28.44,
+            },
+            {
+                "symbol": "SANB11",
+                "amount": 3,
+                "currentPrice": 40.77
+            },
+        ],
+        "consolidated": 413.19 // (234.00 + (28.44 * 2) + (40.77 * 3)
+    }
+    res.json(userPosition);
+}) 
  
 app.post('/auth/login', (req, res, next) => { 
     auth.login(req, res);
