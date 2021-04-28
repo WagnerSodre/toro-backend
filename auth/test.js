28d1c8d1a759126f21efd443c058d3edc9950858
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 03:00:00 -0300

    created verifyJWT test

--- auth/test.js
+++ auth/test.js
@@ -68,3 +68,94 @@ describe('Logout', function() {
     });
   });
 });
+
+describe('verifyJWT', function() {
+  describe('When a user tries to login without a token', function() {
+    it('should give a no token error', function() {
+      let req = {
+        'headers': {}
+      };
+      let res = {
+        send: function(){ },
+        json: function(err){
+            assert.equal(err.message, "No token provided");
+        },
+        status: function(responseStatus) {
+            assert.equal(responseStatus, 401);
+            return this; 
+        }
+      };
+      let valid = false;
+      let next = function() {
+        valid = true;
+      }
+      auth.verifyJWT(req, res, next);
+      assert.equal(valid, false);
+    });
+  });
+
+  describe('When a user tries to login with a invalid token', function() {
+    it('should give a invalid token error', function() {
+      let req = {
+        'headers': {
+          'x-access-token': '1'
+        }
+      };
+      let res = {
+        send: function(){ },
+        json: function(err){
+            assert.equal(err.message, "Failed to authenticate token");
+        },
+        status: function(responseStatus) {
+            assert.equal(responseStatus, 500);
+            return this; 
+        }
+      };
+      let valid = false;
+      let next = function() {
+        valid = true;
+      }
+      auth.verifyJWT(req, res, next);
+      assert.equal(valid, false);
+    });
+  });
+  
+  describe('When a user tries to login with a valid token', function() {
+    it('should pass the validation', function() {
+      let token;
+      let req = {
+        'body': {
+          'user': 'wagner',
+          'pwd': '12345'
+        }
+      };
+      let res = {
+        send: function(){ },
+        json: function(res){
+          token = res.token;
+        },
+        status: function(responseStatus) {
+            return this; 
+        }
+      };
+      auth.login(req, res);
+      req = {
+        'headers': {
+          'x-access-token': token
+        }
+      };
+      res = {
+        send: function(){ },
+        status: function() {
+            return this; 
+        }
+      };
+      let valid = false;
+      let next = function() {
+        valid = true;
+      }
+      auth.verifyJWT(req, res, next);
+      assert.equal(valid, true);
+    });
+  });
+});
