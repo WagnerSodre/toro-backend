bf3c117b44c7e2fb66319474eb80e67b8c79b99e
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 01:34:30 -0300

    created login test

---
+++ auth/test.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const auth = require('./auth');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+describe('Auth', function() {
+  describe('When a invalid user tries to login', function() {
+    it('should return an error', function() {
+      let req = {
+        'body': {
+          'user': 'leonardo',
+          'pwd': '12345'
+        }
+      };
+      let res = {
+        send: function(){ },
+        json: function(err){
+            assert.equal(err.message, 'Invalid user!');
+        },
+        status: function(responseStatus) {
+            assert.equal(responseStatus, 500);
+            return this; 
+        }
+      };
+      auth.login(req, res);
+    });
+  });
+
+  describe('When a valid user tries to login', function() {
+    it('should return a token', function() {
+      let req = {
+        'body': {
+          'user': 'wagner',
+          'pwd': '12345'
+        }
+      };
+      let res = {
+        send: function(){ },
+        json: function(res){
+            assert.equal(res.auth, true);
+        },
+        status: function(responseStatus) {
+            assert.equal(responseStatus, 200);
+            return this; 
+        }
+      };
+      auth.login(req, res);
+    });
+  });
+});
