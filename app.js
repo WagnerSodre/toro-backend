6ebf0d347bea6f750ff8305d687a06ab4a7cc392
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 02:18:06 -0300

    updated login endpoint

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const http = require('http'); 
 const express = require('express'); 
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const auth = require('./auth/auth');
 
 dotenv.config();
 
 const app = express(); 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
  
 app.get('/', (req, res, next) => {
     res.json({message: "API is working"});
 })
  
-app.get('/auth', (req, res, next) => { 
-    
+app.post('/auth/login', (req, res, next) => { 
+    console.log(req);
+    auth.login(req, res);
 }) 
  
 const server = http.createServer(app); 
