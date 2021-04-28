08332fb44fa6a7449855568c2f6311e7ffb86088
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 18:45:26 -0300

    auth now checks the user in the database

--- auth/auth.js
+++ auth/auth.js
@@ -1,18 +1,21 @@
 'use strict';
+
 const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 exports.login = function(req, res){
     //validation should use db
-    if(req.body.user === 'wagner' && req.body.pwd === '12345'){
-      //auth ok
-      const id = 1; //id should come from db
-      let token = jwt.sign({ id }, process.env.SECRET, {
-        expiresIn: 300 // expires in 5min
-      });
-      return res.status(200).json({ auth: true, token: token });
-    }
-    
-    res.status(401).json({message: 'Unauthorized'});
+    User.findOne({name: req.body.user, password: req.body.pwd})
+    .then(user => {
+        //auth ok
+        const id = user._id;
+        let token = jwt.sign({ id }, process.env.SECRET, {
+          expiresIn: 300 // expires in 5min
+        });
+        res.status(200).json({ auth: true, token: token });
+    })
+    .catch(() => res.status(401).json({message: 'Unauthorized'}));
 };
 
 exports.logout = function(req, res){
