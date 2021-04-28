cd3a8f72d1490a8b752b8fbfa8b3a6fdc2e6cbd6
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 02:22:46 -0300

    created logout endpoint

--- app.js
+++ app.js
@@ -15,10 +15,13 @@ app.get('/', (req, res, next) => {
 })
  
 app.post('/auth/login', (req, res, next) => { 
-    console.log(req);
     auth.login(req, res);
 }) 
  
+app.post('/auth/logout', function(req, res) {
+    auth.logout(req, res);
+})
+
 const server = http.createServer(app); 
 
 server.listen(process.env.PORT);
