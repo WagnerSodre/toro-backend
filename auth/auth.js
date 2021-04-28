90b82243dbe5ca65147412240ec9b09482c3ee66
Wagner Sodré <wagnersodre@MacBook-Pro-de-Wagner.local>
2021-04-28 02:20:36 -0300

    implelemted logout function

--- auth/auth.js
+++ auth/auth.js
@@ -2,10 +2,10 @@
 const jwt = require('jsonwebtoken');
 
 exports.login = function(req, res){
-    //esse teste abaixo deve ser feito no seu banco de dados
+    //validation should use db
     if(req.body.user === 'wagner' && req.body.pwd === '12345'){
       //auth ok
-      const id = 1; //esse id viria do banco de dados
+      const id = 1; //id should come from db
       var token = jwt.sign({ id }, process.env.SECRET, {
         expiresIn: 300 // expires in 5min
       });
@@ -13,4 +13,8 @@ exports.login = function(req, res){
     }
     
     res.status(401).json({message: 'Unauthorized'});
+};
+
+exports.logout = function(req, res){
+    res.status(200).send({ auth: false, token: null }); 
 };
